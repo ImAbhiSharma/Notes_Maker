7ba7868dd11e1eb3b5e29765fb72513a58115a2e
ImAbhiSharma <abhi55555as25@gmail.com>
2023-12-08 13:21:10 +0530

    updated

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function header(){
+    return (
+    <header>
+        <h1>
+            Notes Buddy
+        </h1>
+    </header>
+    );
+}
+
+
+export default header;
